bf3f9ee6d7268badda372d40cb09c2a94065bea0
arya <abiyasa1331@gmail.com>
2023-09-27 09:35:15 +0700

    Jurnal-01

---
+++ Jurnal01.java
@@ -0,0 +1,11 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String nama = "Arya Meidi Abiyasa";
+        String nim = "607062300073";
+        String kelas = "D3IF-47-04";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+    } 
+}
